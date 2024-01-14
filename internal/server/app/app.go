b73cc350345c17b517ea904ba3f82b8b75e39ba4
Dmitry <ddaemon@mail.ru>
2024-01-14 21:49:51 +0300

    fix: iter5

--- internal/server/app/app.go
+++ internal/server/app/app.go
@@ -6,7 +6,6 @@ import (
 	"github.com/dnsoftware/go-metrics/internal/server/handlers"
 	"github.com/dnsoftware/go-metrics/internal/storage"
 	"net/http"
-	"os"
 )
 
 func ServerRun() {
@@ -14,10 +13,10 @@ func ServerRun() {
 	endpoint := flag.String("a", "localhost:8080", "server endpoint")
 	flag.Parse()
 
-	runAddr := os.Getenv("ADDRESS")
-	if runAddr != "" {
-		endpoint = &runAddr
-	}
+	//runAddr := os.Getenv("ADDRESS")
+	//if runAddr != "" {
+	//	endpoint = &runAddr
+	//}
 
 	repository := storage.NewMemStorage()
 
