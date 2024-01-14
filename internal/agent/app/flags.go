681def94654cee86b956e2d73283a33dde0eb490
Dmitry <ddaemon@mail.ru>
2024-01-14 19:40:12 +0300

    fix: iter5

--- internal/agent/app/flags.go
+++ internal/agent/app/flags.go
@@ -1,6 +1,10 @@
 package app
 
-import "flag"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+)
 
 var (
 	flagRunAddr        string
@@ -12,9 +16,33 @@ var (
 // и сохраняет их значения в соответствующих переменных
 func parseFlags() {
 
+	type Config struct {
+		runAddr        string `env:"ADDRESS"`
+		reportInterval int64  `env:"REPORT_INTERVAL"`
+		pollInterval   int64  `env:"POLL_INTERVAL"`
+	}
+
+	var cfg Config
+
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.Int64Var(&flagReportInterval, "r", 10, "report interval")
 	flag.Int64Var(&flagPollInterval, "p", 2, "poll interval")
 
 	flag.Parse()
+
+	// переменные окружения
+	if cfg.runAddr != "" {
+		flagRunAddr = cfg.runAddr
+	}
+	if cfg.reportInterval != 0 {
+		flagReportInterval = cfg.reportInterval
+	}
+	if cfg.pollInterval != 0 {
+		flagPollInterval = cfg.pollInterval
+	}
 }
