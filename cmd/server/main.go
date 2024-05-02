7d8792b0905e44a4d2ebba6253812e4295e63cfc
root <ddaemon@mail.ru>
2024-05-03 00:49:11 +0300

    iter20

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/dnsoftware/go-metrics/internal/server/app"
+import (
+	"fmt"
+
+	"github.com/dnsoftware/go-metrics/internal/server/app"
+)
+
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
 
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	app.ServerRun()
 }
