95eb1e6fff9e251a97233680a224defe3e432e61
root <ddaemon@mail.ru>
2024-05-04 10:28:43 +0300

    iter20 tests

--- internal/storage/postgresql_test.go
+++ internal/storage/postgresql_test.go
@@ -16,7 +16,8 @@ func TestNewPostgresqlStorage(t *testing.T) {
 	//ddsn := os.Getenv("DATABASE_DSN")
 	ddsn := cfg.DatabaseDSN
 	if ddsn == "" {
-		ddsn = "postgres://praktikum:praktikum@127.0.0.1:5532/praktikum?sslmode=disable"
+		//ddsn = "postgres://praktikum:praktikum@127.0.0.1:5532/praktikum?sslmode=disable"
+		ddsn = "postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable"
 	}
 
 	pgStorage, err := NewPostgresqlStorage(ddsn)
